60719cd55184e21b0e3edb56588fb6f8724b5019
Dmitrii Kislenko <kislenko1910@mail.ru>
2019-10-25 16:56:19 +0300

    first commit

---
+++ precache-manifest.3ba2c1f3b0b9a8331214a82316a75ec7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83033f38dd84418d273f45f9255df754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cc8b1439fc9b5dd1b00",
+    "url": "/static/css/main.ca345e6c.chunk.css"
+  },
+  {
+    "revision": "cbc4cbba250db240812f",
+    "url": "/static/js/2.01d8447c.chunk.js"
+  },
+  {
+    "revision": "6cc8b1439fc9b5dd1b00",
+    "url": "/static/js/main.c1adb6ab.chunk.js"
+  },
+  {
+    "revision": "42eb98ea172ef39510b9",
+    "url": "/static/js/runtime-main.5441bf2f.js"
+  }
+]);
